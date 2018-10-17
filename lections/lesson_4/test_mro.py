a249951727a92045aeaeca648f2d4f6baf7836c1
Aleksandr Kyrylchuk <alex@livekick.com>
2018-10-17 16:57:36 +0300

    add lesson 3 test script

---
+++ lections/lesson_4/test_mro.py
@@ -0,0 +1,46 @@
+
+class Alive(object):
+    @property
+    def its_me(self):
+        return f"I am {self.__class__.__name__}"
+
+
+class Animal(Alive):
+    pass
+
+
+class Herb(Alive):
+    pass
+
+
+class ComplicatedHerb(Herb):
+    pass
+
+
+class Plant(ComplicatedHerb):
+    pass
+
+
+class UFO:
+    pass
+
+
+class Monkey(Alive, UFO):
+    pass
+
+
+class Human(Monkey):
+    pass
+
+
+class ModernHuman(Human, Plant):
+    pass
+
+
+# print(ModernHuman().its_me)
+print()
+
+for cls in ModernHuman.mro():
+    print(cls.__name__, end=" -> ")
+else:
+    print("AttributeError")
